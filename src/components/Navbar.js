bda26f35c55448ce5ce1c8d8b04146f8046c6ccd
Ramzi Nofal <ramzis.mailbox@gmail.com>
2021-09-09 20:28:07 +0100

    Navbar ready.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 import "./Navbar.css";
+import MyDropdown from "./Dropdown.js";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      logedIn: false,
       menuIconState: false,
     };
 
     this.handleClick = this.handleClick.bind(this);
+    //this.login = this.login.bind(this);
   }
+
+  /* //WHAT IS WRONG WITH THIS????????????
+  login() {
+    this.setState( {logedIn : !this.state.logedIn});
+  }
+
+   <button onClick={this.login}>lol</button>
+   
+*/
 
   handleClick() {
     this.setState({ menuIconState: !this.state.menuIconState });
@@ -21,17 +33,35 @@ class Navbar extends Component {
       {
         className: "nav-links",
         url: "www.google.com",
-        title: "DOG",
+        title: "PROJETCS",
       },
       {
         className: "nav-links",
         url: "www.google.com",
-        title: "DOG2",
+        title: "EDUCATION",
       },
       {
         className: "nav-links",
         url: "www.google.com",
-        title: "DOG3",
+        title: "HOBBIES",
+      },
+    ];
+
+    const logedInMenuItems = [
+      {
+        title: "User",
+        link: " ",
+      },
+      {
+        title: "Logout",
+        link: " ",
+      },
+    ];
+
+    const LogedOutMenuItems = [
+      {
+        title: "Login",
+        link: " ",
       },
     ];
 
@@ -62,7 +92,23 @@ class Navbar extends Component {
                 </li>
               );
             })}
+            <li>
+              <Link className="nav-links log-in-out">
+                {this.state.logedIn ? "LOGOUT" : "LOGIN"}
+              </Link>
+            </li>
           </ul>
+
+          {/* This is a customised Material Ui dropdown menu.*/}
+          <MyDropdown
+            iconClassName={
+              this.state.logedIn ? "far fa-user" : "fas fa-sign-in-alt"
+            }
+            menuItems={
+              this.state.logedIn ? logedInMenuItems : LogedOutMenuItems
+            }
+            isLogedIn={this.state.logedIn}
+          />
         </div>
       </nav>
     );
