cdb58b6f4e01c7f612f1d5eeb9df09091f39e414
Ramzi Nofal <ramzis.mailbox@gmail.com>
2021-10-31 14:51:02 +0000

    Hero, Aboutme, Navbar update.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ function Hero(props) {
   return (
 
     <div className='hero-container'>
-    <video src="/video/molly.mp4" autoPlay loop muted />
+    <video src="/video/molly.mp4" autoPlay loop muted playsinline/>
     <h1>Thank You for Visiting My Site</h1>
  
     
