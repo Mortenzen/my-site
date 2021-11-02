06139f5f4bb814c18bb030cae989f65d443e2485
Ramzi Nofal <ramzis.mailbox@gmail.com>
2021-11-02 22:02:36 +0000

    index.js formated.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import App from "./App";
 import axios from "axios";
 
 /*Here the JWtoken is added to the header. The future requests will be validated with this string.*/
-axios.defaults.headers.common["authorization"] = "Bearer " + localStorage.getItem("token");
+axios.defaults.headers.common["authorization"] =
+  "Bearer " + localStorage.getItem("token");
 
-ReactDOM.render(
-  <App />,
-
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
